1b2f686182ae25391f717aa22fef3a530509762f
vaspihernandez <league@WorthyMini.local>
2017-06-03 11:28:54 -0700

    Add square-4 square -triangle

---
+++ src/day1/robot/graffiti.java
@@ -0,0 +1,41 @@
+package day1.robot;
+
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+
+
+public class graffiti {public static void main(String[] args) {
+	Robot r2d2=new Robot();
+
+	r2d2.setPenColor(Color.MAGENTA);
+	r2d2.penDown();
+	r2d2.setSpeed(10);
+	r2d2.turn(-90);
+	r2d2.move(360);
+	r2d2.turn(-90);
+	r2d2.move(360);
+	r2d2.turn(-90);
+	r2d2.move(360);
+	r2d2.turn(-90);
+	r2d2.move(360);
+	r2d2.turn(-180);
+	r2d2.move(180);
+	r2d2.turn(90);
+	r2d2.move(360);
+	r2d2.turn(90);
+	r2d2.move(180);
+	r2d2.turn(90);
+	r2d2.move(180);
+	r2d2.turn(90);
+	r2d2.move(360);
+	r2d2.turn(90);
+	r2d2.move(180);
+	r2d2.turn(116);
+	r2d2.move(420);
+
+
+}
+
+}
